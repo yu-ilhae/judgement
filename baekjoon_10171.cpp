507b44ecb0cf3e9a339c4e405fda3c5b0c1f8766
yuilhae <62281088+yuilhae@users.noreply.github.com>
2020-04-24 20:52:02 +0900

    Create baekjoon_10171.cpp
    try_1:
    success

---
+++ baekjoon_10171.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|";
+    return 0;
+
+}
