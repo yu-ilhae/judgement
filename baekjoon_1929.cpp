6e0883c7d71934aa5963760c697a3df07f5e1855
yuilhae <62281088+yuilhae@users.noreply.github.com>
2020-04-28 20:39:28 +0900

    online judge

---
+++ baekjoon_1929.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+bool isComposite(const int& n);
+using namespace std;
+int main() {
+	int in_min, in_max;
+
+	cin >> in_min >> in_max;
+	for (int i = in_min; i <= in_max; i++) {
+		if (isComposite(i) == true) {
+			cout << i << endl;
+		}
+	}
+
+	return 0;
+}
+bool isComposite(const int & n) {
+	if (n == 1) return false;
+	int check_point = (int)ceil(sqrt(n));
+	for (int i = 2; i <= check_point; i++) {
+		if (n % i == 0 && i <= check_point) return false;
+	}
+	return true;
+}
---
+++ baekjoon_19469092.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int up, down, total;
+	int day = 1;
+
+	cin >> up >> down >> total;
+	total -= up;
+	day += total / (up - down);
+	if (total % (up - down) != 0) day++;
+	cout << day;
+}
